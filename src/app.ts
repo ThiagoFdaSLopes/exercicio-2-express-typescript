a56256691cf618cb17bb003132bb19a36e76fc96
Thiago <programadorthiagolopes@gmail.com>
2023-02-04 15:43:41 -0300

    feat: Criado rota login

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import routerLogin from './routes/login';
 
 const app = express();
 
 app.use(express.json());
+app.use('/login', routerLogin);
 
 export default app;
